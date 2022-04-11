a7d02fa8a3f60dac540bb2a3d9877b2e9b5cb0d6
fondarda <alexis.fondard@etu.umontpellier.fr>
2022-04-11 09:28:11 +0200

    boucle sur client1 et serveur fonctionnelle

--- serveur.c
+++ serveur.c
@@ -6,61 +6,61 @@
 
 // argv[1] = port
 
-int main(int argc, char *argv[]) {
-  
+int main(int argc, char *argv[])
+{
+
   printf("Début programme\n");
 
   int dS = socket(PF_INET, SOCK_STREAM, 0);
+  if (dS == -1)
+  {
+    perror("Problème de création de socket serveur");
+    return 0;
+  }
   printf("Socket Créé\n");
-
 
   struct sockaddr_in ad;
   ad.sin_family = AF_INET;
-  ad.sin_addr.s_addr = INADDR_ANY ;
-  ad.sin_port = htons(atoi(argv[1])) ;
-  bind(dS, (struct sockaddr*)&ad, sizeof(ad)) ;
+  ad.sin_addr.s_addr = INADDR_ANY;
+  ad.sin_port = htons(atoi(argv[1]));
+  bind(dS, (struct sockaddr *)&ad, sizeof(ad));
   printf("Socket Nommé\n");
 
-  listen(dS, 7) ;
+  listen(dS, 7);
   printf("Mode écoute\n");
 
-  struct sockaddr_in aC ;
-  socklen_t lg = sizeof(struct sockaddr_in) ;
-  int dSC = accept(dS, (struct sockaddr*) &aC,&lg);
-  if (dSC < 0) {
-	  perror("Erreur lors de l'acceptation du client\n");
+  struct sockaddr_in aC;
+  socklen_t lg = sizeof(struct sockaddr_in);
+  int dSC = accept(dS, (struct sockaddr *)&aC, &lg);
+  printf("Client Connecté\n");
+
+  int taille = -1;
+  int r = 10;
+  while (1)
+  {
+    if (recv(dSC, &taille, sizeof(int), 0) < 0 || taille == -1)
+    {
+      perror("Problème de réception de taille depuis le serveur");
+      return 0;
+    }
+    printf("Taille reçue : %d\n", taille);
+
+    char msg[taille];
+    if (recv(dSC, msg, taille, 0) < 0)
+    {
+      perror("Problème de réception du message depuis le serveur");
+      return 0;
+    }
+    printf("Message reçu : %s\n", msg);
+
+    if (send(dSC, &r, sizeof(int) + 1, 0) < 0)
+    {
+      perror("Problème d'envoi depuis le serveur");
+      return 0;
+    }
+    printf("Message Envoyé\n");
   }
-   printf("Client Connecté\n");
-   int taille_chaine;
-	while(0) {
-		
-		int error_recv = recv(dSC, &taille_chaine, sizeof(int), 0);
-		if (error_recv < 0) {
-			perror("Erreur lors de la réception du message\n");
-			return 0;
-		}
-		printf("Message reçu : %d\n", taille_chaine) ;
-
-
-		char *msg = malloc(sizeof(char)*taille_chaine);
-		int error_recv2 = recv(dSC, msg, taille_chaine, 0);
-		if (error_recv2 < 0) {
-			perror("Erreur lors de la réception du message\n");
-			return 0;
-		}
-		printf("Message reçu : %s\n", msg) ;
-		free(msg);
-		
-		int r = 10 ;
-		
-		int error_send = send(dSC, &r, sizeof(int), 0);
-		if (error_send < 0) {
-			perror("Le message n'a pas pu être envoyé\n");
-			return 0;
-		}
-		printf("Message Envoyé\n");
-	}
-  shutdown(dSC, 2) ; 
-  shutdown(dS, 2) ;
+  shutdown(dSC, 2);
+  shutdown(dS, 2);
   printf("Fin du programme\n");
 }
