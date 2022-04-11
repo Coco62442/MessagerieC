8add1d93f87223bbe27de611b7f8fac7801bb8e7
laura.benaiton <laura.benaiton@igtp3-pc15.isim.intra>
2022-04-11 09:35:34 +0200

    init

--- client1.c
+++ client1.c
@@ -9,51 +9,51 @@
 
 int main(int argc, char *argv[]) {
 
-  printf("Début programme\n");
-  int dS = socket(PF_INET, SOCK_STREAM, 0);
-  printf("Socket Créé\n");
+	printf("Début programme\n");
+	int dS = socket(PF_INET, SOCK_STREAM, 0);
+	printf("Socket Créé\n");
 
-  // Message a envoyé
-  
-  char * chaine = malloc(10*sizeof(char));
-  printf("Entrer un mot de 9 caractères maximum\n");
-  fgets(chaine, 10, stdin);
+	// Message a envoyé
 
-  struct sockaddr_in aS;
-  aS.sin_family = AF_INET;
-  inet_pton(AF_INET,argv[1],&(aS.sin_addr));
-  aS.sin_port = htons(atoi(argv[2])) ;
-  socklen_t lgA = sizeof(struct sockaddr_in) ;
-  int error_connect = connect(dS, (struct sockaddr *) &aS, lgA);
-  if (error_connect < 0) {
-	  perror("Erreur lors de la connection au serveur\nVérifier l\'adresse IP fournie");
-	  return 0;
-  };
-  printf("Socket Connecté\n");
-  int taille_chaine = strlen(chaine);
-	while (0) {
-		
-		int error_send = send(dS, &taille_chaine, sizeof(int) , 0) ;
-		if (error_send < 0) {
-			perror("Erreur lors de l\'envoie du message au serveur");
-		};
-		printf("Message Envoyé \n");
+	char * chaine = malloc(10*sizeof(char));
+	printf("Entrer un mot de 9 caractères maximum\n");
+	fgets(chaine, 10, stdin);
 
-
-		int error_send2 = send(dS, chaine, strlen(chaine) + 1 , 0) ;
-		if (error_send2 < 0) {
-			perror("Erreur lors de l\'envoie du message au serveur");
-		};
-		printf("Message Envoyé \n");
-
-		int r;
-		int error_recv = recv(dS, &r, sizeof(int), 0);
-		if (error_recv < 0) {
-			perror("Erreur le message n\'a pas été reçu");
-		};
-		printf("Réponse reçue : %d\n", r) ;
+	struct sockaddr_in aS;
+	aS.sin_family = AF_INET;
+	inet_pton(AF_INET,argv[1],&(aS.sin_addr));
+	aS.sin_port = htons(atoi(argv[2])) ;
+	socklen_t lgA = sizeof(struct sockaddr_in) ;
+	int error_connect = connect(dS, (struct sockaddr *) &aS, lgA);
+	if (error_connect < 0) {
+		perror("Erreur lors de la connection au serveur\nVérifier l\'adresse IP fournie");
+		return 0;
 	};
-  free(chaine);
-  shutdown(dS,2) ;
-  printf("Fin du programme\n");
+	printf("Socket Connecté\n");
+	int taille_chaine = strlen(chaine);
+
+
+	int error_send = send(dS, &taille_chaine, sizeof(int) , 0) ;
+	if (error_send < 0) {
+		perror("Erreur lors de l\'envoie du message au serveur");
+	};
+	printf("Message Envoyé \n");
+
+
+	int error_send2 = send(dS, chaine, strlen(chaine) + 1 , 0) ;
+	if (error_send2 < 0) {
+		perror("Erreur lors de l\'envoie du message au serveur");
+	};
+	printf("Message Envoyé \n");
+
+	int r;
+	int error_recv = recv(dS, &r, sizeof(int), 0);
+	if (error_recv < 0) {
+		perror("Erreur le message n\'a pas été reçu");
+	};
+	printf("Réponse reçue : %d\n", r) ;
+
+	free(chaine);
+	shutdown(dS,2) ;
+	printf("Fin du programme\n");
 }
