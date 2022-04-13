4b53d5e1efb57b284118100975a6f85160955ba7
fondarda <alexis.fondard@etu.umontpellier.fr>
2022-04-13 10:51:22 +0200

    avancée client 2

--- client2.c
+++ client2.c
@@ -4,56 +4,61 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LENGTH 10
+
 // argv[1] = adresse ip
 // argv[2] = port
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
   printf("Début programme\n");
   int dS = socket(PF_INET, SOCK_STREAM, 0);
+  if (dS == -1)
+  {
+    perror("Problème de création de socket client");
+    return 0;
+  }
   printf("Socket Créé\n");
-
-  // Message a envoyé
-  
-  char * chaine = malloc(10*sizeof(char));
-  printf("Entrer un mot de 9 caractères maximum\n");
-  fgets(chaine, 10, stdin);
 
   struct sockaddr_in aS;
   aS.sin_family = AF_INET;
-  inet_pton(AF_INET,argv[1],&(aS.sin_addr));
-  aS.sin_port = htons(atoi(argv[2])) ;
-  socklen_t lgA = sizeof(struct sockaddr_in) ;
-  int error_connect = connect(dS, (struct sockaddr *) &aS, lgA);
-  if (error_connect < 0) {
-	  perror("Erreur lors de la connection au serveur\nVérifier l\'adresse IP fournie");
-	  return 0;
-  };
+  inet_pton(AF_INET, argv[1], &(aS.sin_addr));
+  aS.sin_port = htons(atoi(argv[2]));
+  socklen_t lgA = sizeof(struct sockaddr_in);
+  connect(dS, (struct sockaddr *)&aS, lgA);
   printf("Socket Connecté\n");
-  int taille_chaine = strlen(chaine);
-	while (0) {
-		
-		int error_send = send(dS, &taille_chaine, sizeof(int) , 0) ;
-		if (error_send < 0) {
-			perror("Erreur lors de l\'envoie du message au serveur");
-		};
-		printf("Message Envoyé \n");
 
+  char *buffer = (char *)malloc(MAX_LENGTH);
+  int taille = 0;
+  while (1)
+  {
+    printf("Entrer un message de taille max %d: \n", MAX_LENGTH - 1);
+    fgets(buffer, MAX_LENGTH, stdin);
+    taille = strlen(buffer) + 1;
 
-		int error_send2 = send(dS, chaine, strlen(chaine) + 1 , 0) ;
-		if (error_send2 < 0) {
-			perror("Erreur lors de l\'envoie du message au serveur");
-		};
-		printf("Message Envoyé \n");
+    if (send(dS, &taille, sizeof(int), 0) < 0)
+    {
+      perror("Problème d'envoi de la taille");
+      return 0;
+    }
+    printf("Taille Envoyée \n");
 
-		int r;
-		int error_recv = recv(dS, &r, sizeof(int), 0);
-		if (error_recv < 0) {
-			perror("Erreur le message n\'a pas été reçu");
-		};
-		printf("Réponse reçue : %d\n", r) ;
-	};
-  free(chaine);
-  shutdown(dS,2) ;
+    if (send(dS, buffer, taille, 0) < 0)
+    {
+      perror("Problème d'envoi du message");
+      return 0;
+    }
+    printf("Message Envoyé \n");
+
+    int r;
+    if (recv(dS, &r, sizeof(int), 0) < 0)
+    {
+      perror("Problème de réception du client");
+      return 0;
+    }
+    printf("Réponse reçue : %d\n", r);
+  }
+  shutdown(dS, 2);
   printf("Fin du programme\n");
 }
