060f8f1e9dd0a91da6a42fd534aa1cafe3f42637
BENAITON Laura <laura.benaiton@igtp3-pc14.isim.intra>
2022-04-19 15:05:20 +0200

    recup main

--- serveur.c
+++ serveur.c
@@ -12,73 +12,6 @@ struct args {
 
 // argv[1] = port
 
-<<<<<<< HEAD
-int main(int argc, char *argv[]) {
-
-	printf("Début programme\n");
-
-	int dS = socket(PF_INET, SOCK_STREAM, 0);
-	printf("Socket 1 Créé\n");
-
-	struct sockaddr_in ad;
-	ad.sin_family = AF_INET;
-	ad.sin_addr.s_addr = INADDR_ANY ;
-	ad.sin_port = htons(atoi(argv[1])) ;
-	bind(dS, (struct sockaddr*)&ad, sizeof(ad)) ;
-	printf("Socket Nommé\n");
-
-	listen(dS, 7) ;
-	printf("Mode écoute\n");
-
-	struct sockaddr_in aC ;
-	socklen_t lg = sizeof(struct sockaddr_in) ;
-	// creation des sockets
-	int dSC1 = accept(dS, (struct sockaddr*) &aC,&lg);
-	if (dSC1 < 0) {
-		perror("Erreur lors de l'acceptation du premier client\n");
-	}
-	printf("Client 1 Connecté\n");
-	int dSC2 = accept(dS, (struct sockaddr*) &aC,&lg);
-	if (dSC2 < 0) {
-		perror("Erreur lors de l'acceptation du second client\n");
-	}
-	printf("Client 2 Connecté\n");
-
-	// reception des messages
-	int taille_chaine;
-	
-	int error_recv = recv(dSC1, &taille_chaine, sizeof(int), 0);
-	if (error_recv < 0) {
-		perror("Erreur lors de la réception du message\n");
-		return 0;
-	}
-	printf("Message reçu : %d\n", taille_chaine) ;
-
-
-	char msg = malloc(sizeof(char)*taille_chaine);
-	int error_recv2 = recv(dSC1, msg, taille_chaine, 0);
-	if (error_recv2 < 0) {
-		perror("Erreur lors de la réception du message\n");
-		return 0;
-	}
-	printf("Message reçu : %s\n", msg) ;
-	// renvoie du message
-
-
-
-	free(msg);
-
-	int r = 10 ;
-	
-	int error_send = send(dSC2, &r, sizeof(int), 0);
-	if (error_send < 0) {
-		perror("Le message n'a pas pu être envoyé\n");
-		return 0;
-	}
-	printf("Message Envoyé\n");
-		
-	shutdown(dSC1, 2) ; 
-=======
 void *client1To2(void * arg) {
 	int dSC1 = ((struct args*)arg)->adrC1;
 	int dSC2 = ((struct args*)arg)->adrC2;
@@ -202,7 +135,6 @@ int main(int argc, char *argv[]) {
 
 	shutdown(dSC1, 2) ;
 	shutdown(dSC2, 2) ;
->>>>>>> cb6a9977ad52a025f512c603fd348d530df9303d
 	shutdown(dS, 2) ;
 	printf("Fin du programme\n");
 }
