c3c4346c12a9917ef6f53039089ec6b183cedcc2
Coco62442 <corentinclement10@gmail.com>
2022-04-30 12:10:40 +0200

    Maj du main(Corentin)

--- client.c
+++ client.c
@@ -4,94 +4,201 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <signal.h>
+#include <unistd.h>
+// ################################## Ne sers a rien ################################################################
+#define MAX_LENGTH 10
+
+int isEnd = 0;
+int dS = -1;
+int boolConnect = 0;
+
+/*
+ * Vérifie si un client souhaite quitter la communication
+ * Paramètres : char ** msg : message du client à vérifier
+ * Retour : 1 (vrai) si le client veut quitter, 0 (faux) sinon
+ */
+int endOfCommunication(char *msg)
+{
+	if (strcmp(msg, "/fin\n") == 0)
+	{
+		strcpy(msg, "** a quitté la communication **\n");
+		return 1;
+	}
+	return 0;
+}
+
+/*
+ * Envoie un message à une socket et teste que tout se passe bien
+ * Paramètres : int dS : la socket
+ *              char * msg : message à envoyer
+ */
+void sending(char *msg)
+{
+	if (send(dS, msg, strlen(msg) + 1, 0) == -1)
+	{
+		perror("Erreur au send");
+		exit(-1);
+	}
+}
+
+/*
+ * Fonction pour le thread d'envoi
+ */
+void *sendingForThread()
+{
+	while (!isEnd)
+	{
+		/*Saisie du message au clavier*/
+		char *m = (char *)malloc(sizeof(char) * 100);
+		printf(">");
+		fgets(m, 100, stdin);
+
+		// On vérifie si le client veut quitter la communication
+		isEnd = endOfCommunication(m);
+
+		// Envoi
+		sending(m);
+		free(m);
+	}
+	shutdown(dS, 2);
+	return NULL;
+}
+
+/*
+ * Receptionne un message d'une socket et teste que tout se passe bien
+ * Paramètres : int dS : la socket
+ *              char * msg : message à recevoir
+ *              ssize_t size : taille maximum du message à recevoir
+ */
+void receiving(char *rep, ssize_t size)
+{
+	if (recv(dS, rep, size, 0) == -1)
+	{
+		printf("** fin de la communication **\n");
+		exit(-1);
+	}
+}
+
+/*
+* Fonction pour le thread de réception
+*/
+void *receivingForThread()
+{
+	while (!isEnd)
+	{
+		char *r = (char *)malloc(sizeof(char) * 100);
+		receiving(r, sizeof(char) * 100);
+
+		printf(">%s", r);
+		free(r);
+	}
+	shutdown(dS, 2);
+	return NULL;
+}
+
+/* Signal Handler for SIGINT */
+void sigintHandler(int sig_num)
+{
+    printf("\nFin du programme avec Ctrl + C \n");
+    if(!boolConnect){
+        char *myPseudoEnd = (char *)malloc(sizeof(char) * 12);
+        myPseudoEnd = "FinClient";
+        sending(myPseudoEnd);
+    } 
+    sleep(0.2);
+    sending("** a quitté la communication **\n");
+    exit(1);
+}
 
 // argv[1] = adresse ip
 // argv[2] = port
-
-void *envoieMsg(void * arg) {
-	int dS = (int) arg;
-	char * chaine = malloc(200*sizeof(char));
-	while (1) {
-		// Message a envoyé
-
-		printf("Entrer votre message\n");
-		fgets(chaine, 200, stdin);
-
-		int tailleChaineEnvoie = strlen(chaine);
-			
-		int error_send = send(dS, &tailleChaineEnvoie, sizeof(int) , 0) ;
-		if (error_send < 0) {
-			perror("Erreur lors de l\'envoie du message au serveur");
-		};
-		printf("Message Envoyé \n");
-
-
-		int error_send2 = send(dS, chaine, strlen(chaine) + 1 , 0) ;
-		if (error_send2 < 0) {
-			perror("Erreur lors de l\'envoie du message au serveur");
-		};
-		printf("Message Envoyé \n");
-
-	};
-	free(chaine);
-	int pthread_cancel(pthread_t thread_recu);
-	pthread_exit(NULL);
-}
-
-
-
-void *recuMsg(void * arg) {
-	int dS = (int) arg;	
-	char * rep;
-	while (1) {
-		int tailleChaineRecu;
-		int error_recv1 = recv(dS, &tailleChaineRecu, sizeof(int), 0);
-		if (error_recv1 < 0) {
-			perror("Erreur le message n\'a pas été reçu");
-		};
-		printf("Réponse reçue : %d\n", tailleChaineRecu);
-
-
-		rep = malloc(sizeof(char)*tailleChaineRecu);
-		int error_recv2 = recv(dS, rep, sizeof(char)*tailleChaineRecu, 0);
-		if (error_recv2 < 0) {
-			perror("Erreur le message n\'a pas été reçu");
-		};
-		printf("Réponse reçue : %s\n", rep) ;
-
-		
-	};
-	free(rep);
-	int pthread_cancel(pthread_t thread_envoie);
-	pthread_exit(NULL);
-}
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		perror("Erreur : Lancez avec ./client [votre_ip] [votre_port] ");
+		return -1;
+	}
 	printf("Début programme\n");
-	int dS = socket(PF_INET, SOCK_STREAM, 0);
+
+	// Création de la socket
+	dS = socket(PF_INET, SOCK_STREAM, 0);
+	if (dS == -1)
+	{
+		perror("Problème de création de socket client\n");
+		return -1;
+	}
 	printf("Socket Créé\n");
 
+	// Nommage de la socket
 	struct sockaddr_in aS;
 	aS.sin_family = AF_INET;
-	inet_pton(AF_INET,argv[1],&(aS.sin_addr));
-	aS.sin_port = htons(atoi(argv[2])) ;
-	socklen_t lgA = sizeof(struct sockaddr_in) ;
-	int error_connect = connect(dS, (struct sockaddr *) &aS, lgA);
-	if (error_connect < 0) {
-		perror("Erreur lors de la connection au serveur\nVérifier l\'adresse IP fournie");
-		return 0;
-	};
-	printf("Socket Connecté\n");
+	inet_pton(AF_INET, argv[1], &(aS.sin_addr));
+	aS.sin_port = htons(atoi(argv[2]));
+	socklen_t lgA = sizeof(struct sockaddr_in);
 
-	pthread_t thread_envoie;
-	pthread_t thread_recu;
-	pthread_create(&thread_envoie, NULL, envoieMsg, (void *) (int) dS);
-	pthread_create(&thread_recu, NULL, recuMsg, (void *) (int) dS);
+	// Envoi d'une demande de connexion
+	printf("Connection en cours...\n");
+	if (connect(dS, (struct sockaddr *)&aS, lgA) < 0)
+	{
+		perror("Problème de connexion au serveur\n");
+		exit(-1);
+	}
+	printf("Socket connectée\n");
+
+	// Fin avec Ctrl + C
+    signal(SIGINT, sigintHandler);
+
+	// Saisie du pseudo du client au clavier
+	char *myPseudo = (char *)malloc(sizeof(char) * 12);
+	printf("Votre pseudo (maximum 11 caractères):\n");
+	fgets(myPseudo, 12, stdin);
+
+	// Envoie du pseudo
+	sending(myPseudo);
+
+	char *repServeur = (char *)malloc(sizeof(char) * 60);
+	//Récéption de la réponse du serveur
+	receiving(repServeur, sizeof(char)*60);
+	printf("%s\n", repServeur);
+
+	while (strcmp(repServeur, "Pseudo déjà existant\n") == 0)
+	{
+		// Saisie du pseudo du client au clavier
+		printf("Votre pseudo (maximum 11 caractères)\n");
+		fgets(myPseudo, 12, stdin);
+
+		// Envoie du pseudo
+		sending(myPseudo);
+
+		//Récéption de la réponse du serveur
+		receiving(repServeur, sizeof(char)*60);
+		printf("%s\n", repServeur);
+	}
+	free(myPseudo);
+	boolConnect = 1;
 	
 
-	pthread_join(thread_envoie,0);
-	pthread_join(thread_recu,0);
-	
+	//_____________________ Communication _____________________
+	// Création des threads
+	pthread_t thread_sendind;
+	pthread_t thread_receiving;
+
+	if (pthread_create(&thread_sendind, NULL, sendingForThread, 0) < 0)
+	{
+		perror("Erreur de création de thread d'envoi client\n");
+		exit(-1);
+	}
+
+	if (pthread_create(&thread_receiving, NULL, receivingForThread, 0) < 0)
+	{
+		perror("Erreur de création de thread réception client\n");
+		exit(-1);
+	}
+
+	pthread_join(thread_sendind, NULL);
+	pthread_join(thread_receiving, NULL);
+
 	printf("Fin du programme\n");
-	return 0;
 }
--- serveur.c
+++ serveur.c
@@ -3,138 +3,349 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-
-struct args {
-    int adrC1;
-    int adrC2;
+/*
+ * Définition d'une structure Client pour regrouper toutes les informations du client
+ */
+typedef struct Client Client;
+struct Client
+{
+	int isOccupied;
+	long dSC;
+	char *pseudo;
 };
 
-// argv[1] = port
+/*
+ * - MAX_CLIENT = nombre maximum de clients acceptés sur le serveur
+ * - tabClient = tableau répertoriant les clients connectés
+ * - tabThread = tableau des threads associés au traitement de chaque client
+ * - nbClient = nombre de clients actuellement connectés
+ */
+#define MAX_CLIENT 3
+Client tabClient[MAX_CLIENT];
+pthread_t tabThread[MAX_CLIENT];
+long nbClient = 0;
 
-void *client1To2(void * arg) {
-	int dSC1 = ((struct args*)arg)->adrC1;
-	int dSC2 = ((struct args*)arg)->adrC2;
-	int taille_chaine;
-	while(1) {
-		int error_recv1 = recv(dSC1, &taille_chaine, sizeof(int), 0);
-		if (error_recv1 < 0) {
-			perror("Erreur lors de la réception du message\n");
-			return 0;
-		}
-		printf("Message reçu : %d\n", taille_chaine) ;
+// Création du sémaphore
+sem_t semaphore;
+// Création du mutex pour la modification de tabClient[]
+pthread_mutex_t mutex;
 
-		int error_send1 = send(dSC2, &taille_chaine, sizeof(int), 0);
-		if (error_send1 < 0) {
-			perror("Le message n'a pas pu être envoyé\n");
-			return 0;
+/*
+ * Fonctions pour gérer les indices du tableaux de clients
+ * Retour : un entier, indice du premier emplacement disponible
+ *          -1 si tout les emplacements sont occupés.
+ */
+int giveNumClient()
+{
+	int i = 0;
+	while (i < MAX_CLIENT)
+	{
+		if (!tabClient[i].isOccupied)
+		{
+			return i;
 		}
-		printf("Message Envoyé\n");
-
-		char *msg = malloc(sizeof(char)*taille_chaine);
-		int error_recv2 = recv(dSC1, msg, taille_chaine, 0);
-		if (error_recv2 < 0) {
-			perror("Erreur lors de la réception du message\n");
-			return 0;
-		}
-		printf("Message reçu : %s\n", msg) ;
-		
-		int error_send2 = send(dSC2, msg, sizeof(char)*taille_chaine, 0);
-		if (error_send2 < 0) {
-			perror("Le message n'a pas pu être envoyé\n");
-			return 0;
-		}
-		printf("Message Envoyé\n");
+		i += 1;
 	}
-	int pthread_cancel(pthread_t thread_envoie);
-	pthread_exit(NULL);
+	exit(-1);
 }
 
-void *client2To1(void * arg) {
-	int dSC1 = ((struct args*)arg)->adrC1;
-	int dSC2 = ((struct args*)arg)->adrC2;
-	int taille_chaine;
-	while (1) {
-		int error_recv1 = recv(dSC2, &taille_chaine, sizeof(int), 0);
-		if (error_recv1 < 0) {
-			perror("Erreur lors de la réception test du message\n");
-			return 0;
+/*
+ * Fonctions pour vérifier que le pseudo est unique
+ * Retour : un entier
+ *          1 si le pseudo existe déjà
+ *          0 si le pseudo n'existe pas.
+ */
+int verifPseudo(char *pseudo)
+{
+	int i = 0;
+	while (i < MAX_CLIENT)
+	{
+		if (tabClient[i].isOccupied && strcmp(pseudo, tabClient[i].pseudo) == 0) {
+			return 1;
 		}
-		printf("Message reçu : %d\n", taille_chaine) ;
-
-		int error_send1 = send(dSC1, &taille_chaine, sizeof(int), 0);
-		if (error_send1 < 0) {
-			perror("Le message n'a pas pu être envoyé\n");
-			return 0;
-		}
-		printf("Message Envoyé\n");
-
-		char *msg = malloc(sizeof(char)*taille_chaine);
-		int error_recv2 = recv(dSC2, msg, taille_chaine, 0);
-		if (error_recv2 < 0) {
-			perror("Erreur lors de la réception du message\n");
-			return 0;
-		}
-		printf("Message reçu : %s\n", msg) ;
 		
-		int error_send2 = send(dSC1, msg, sizeof(char)*taille_chaine, 0);
-		if (error_send2 < 0) {
-			perror("Le message n'a pas pu être envoyé\n");
-			return 0;
-		}
-		printf("Message Envoyé\n");
-	};
-	int pthread_cancel(pthread_t thread_client1To2);
-	pthread_exit(NULL);
+		i++;
+	}
+	return 0;
 }
 
-int main(int argc, char *argv[]) {
+/*
+ * Envoie un message à toutes les sockets présentes dans le tableau des clients
+ * et teste que tout se passe bien
+ * Paramètres : int dS : expéditeur du message
+ *              char *msg : message à envoyer
+ */
+void sending(int dS, char *msg)
+{
+    for (int i = 0; i < MAX_CLIENT; i++)
+    {
+        // On n'envoie pas au client qui a écrit le message
+        if (tabClient[i].isOccupied && dS != tabClient[i].dSC)
+        {
+            if (send(tabClient[i].dSC, msg, strlen(msg) + 1, 0) == -1)
+            {
+                perror("Erreur au send");
+                exit(-1);
+            }
+        }
+    }
+}
 
+/*
+ * Envoie un message en mp à un client en particulier
+ * et teste que tout se passe bien
+ * Paramètres : int dSC : destinataire du msg
+ *              char *msg : message à envoyer
+ */
+void sendingDM(char *pseudoReceiver, char *msg)
+{
+    int i = 0;
+    while (i < MAX_CLIENT && tabClient[i].isOccupied && strcmp(tabClient[i].pseudo, pseudoReceiver) != 0)
+    {
+        i++;
+    }
+    if (i == MAX_CLIENT)
+    {
+        perror("Pseudo pas trouvé");
+        exit(-1);
+    }
+    long dSC = tabClient[i].dSC;
+    if (send(dSC, msg, strlen(msg) + 1, 0) == -1)
+    {
+        perror("Erreur à l'envoi du mp");
+        exit(-1);
+    }
+}
+
+/*
+ * Receptionne un message d'une socket et teste que tout se passe bien
+ * Paramètres : int dS : la socket
+ *              char * msg : message à recevoir
+ *              ssize_t size : taille maximum du message à recevoir
+ */
+void receiving(int dS, char *rep, ssize_t size)
+{
+	if (recv(dS, rep, size, 0) == -1)
+	{
+		perror("Erreur au recv");
+		exit(-1);
+	}
+}
+
+/*
+ * Vérifie si un client souhaite quitter la communication
+ * Paramètres : char ** msg : message du client à vérifier
+ * Retour : 1 (vrai) si le client veut quitter, 0 (faux) sinon
+ */
+int endOfCommunication(char *msg)
+{
+	if (strcmp(msg, "** a quitté la communication **\n") == 0)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+/*
+ * Vérifie si un client souhaite utiliser une des commandes
+ * Paramètres : char *msg : message du client à vérifier
+ * Retour : 1 (vrai) si le client utilise une commande, 0 (faux) sinon
+ */
+int useOfCommand(char *msg, char *pseudoSender)
+{
+    char *strToken = strtok(msg, " ");
+    if (strcmp(strToken, "/mp") == 0)
+    {
+        // Récupération du pseudo à qui envoyer le mp
+        char *pseudoReceiver = (char *)malloc(sizeof(char) * 100);
+        pseudoReceiver = strtok(NULL, " ");
+        if (pseudoReceiver == NULL)
+        {
+            printf("Commande \"/mp\" mal utilisée\n");
+            return 0;
+        }
+        char *msg = (char *)malloc(sizeof(char) * 115);
+        msg = strtok(NULL, " ");
+        if (msg == NULL)
+        {
+            printf("Commande \"/mp\" mal utilisée\n");
+            return 0;
+        }
+        // Préparation du message à envoyer
+        char *msgToSend = (char *)malloc(sizeof(char) * 115);
+        strcat(msgToSend, pseudoSender);
+        strcat(msgToSend, " vous chuchotte : ");
+        strcat(msgToSend, msg);
+
+        // Envoi du message au destinataire
+        printf("Envoi du message de %s au clients %s.\n", pseudoSender, pseudoReceiver);
+        sendingDM(pseudoReceiver, msgToSend);
+        return 1;
+    }
+    return 0;
+}
+
+/*
+ * Start routine de pthread_create()
+ */
+void *communication(void *clientParam)
+{
+	int isEnd = 0;
+	int numClient = (long)clientParam;
+	char *pseudoSender = tabClient[numClient].pseudo;
+
+	while (!isEnd)
+	{
+		// Réception du message
+		char *msgReceived = (char *)malloc(sizeof(char) * 100);
+		receiving(tabClient[numClient].dSC, msgReceived, sizeof(char) * 100);
+		printf("\nMessage recu: %s \n", msgReceived);
+
+		// On verifie si le client veut terminer la communication
+		isEnd = endOfCommunication(msgReceived);
+
+		// On vérifie si le client utilise une des commandes
+		char *msgToVerif = (char *)malloc(sizeof(char) * strlen(msgReceived));
+		strcpy(msgToVerif, msgReceived);
+        if (useOfCommand(msgToVerif, pseudoSender))
+        {
+            continue;
+        }
+
+		// Ajout du pseudo de l'expéditeur devant le message à envoyer
+		char *msgToSend = (char *)malloc(sizeof(char) * 115);
+		strcat(msgToSend, pseudoSender);
+		strcat(msgToSend, " : ");
+		strcat(msgToSend, msgReceived);
+
+		// Envoi du message aux autres clients
+		printf("Envoi du message aux %ld clients. \n", nbClient-1);
+		sending(tabClient[numClient].dSC, msgToSend);
+	}
+
+	// Fermeture du socket client
+	pthread_mutex_lock(&mutex);
+	nbClient = nbClient - 1;
+	tabClient[numClient].isOccupied = 0;
+	pthread_mutex_unlock(&mutex);
+	shutdown(tabClient[numClient].dSC, 2);
+	
+	// On relache le sémaphore
+    sem_post(&semaphore);
+
+	return NULL;
+}
+
+/*
+ * _____________________ MAIN _____________________
+ */
+// argv[1] = port
+int main(int argc, char *argv[])
+{
+	// Verification du nombre de paramètres
+	if (argc < 2)
+	{
+		perror("Erreur : Lancez avec ./serveur [votre_port] ");
+		exit(-1);
+	}
 	printf("Début programme\n");
 
+	// Création de la socket
 	int dS = socket(PF_INET, SOCK_STREAM, 0);
+	if (dS < 0)
+	{
+		perror("Problème de création de socket serveur");
+		exit(-1);
+	}
 	printf("Socket Créé\n");
 
-
+	// Nommage de la socket
 	struct sockaddr_in ad;
 	ad.sin_family = AF_INET;
-	ad.sin_addr.s_addr = INADDR_ANY ;
-	ad.sin_port = htons(atoi(argv[1])) ;
-	bind(dS, (struct sockaddr*)&ad, sizeof(ad)) ;
-	printf("Socket Nommé\n");
+	ad.sin_addr.s_addr = INADDR_ANY;
+	ad.sin_port = htons(atoi(argv[1]));
+	if (bind(dS, (struct sockaddr *)&ad, sizeof(ad)) < 0)
+	{
+		perror("Erreur lors du nommage de la socket");
+		exit(-1);
+	}
+	printf("Socket nommée\n");
 
-	listen(dS, 7);
+	// Initialisation du sémaphore
+	sem_init(&semaphore, PTHREAD_PROCESS_SHARED, MAX_CLIENT);
+
+	// Passage de la socket en mode écoute
+	if (listen(dS, 7) < 0)
+	{
+		perror("Problème au niveau du listen");
+		exit(-1);
+	}
 	printf("Mode écoute\n");
 
-	struct sockaddr_in aC ;
-	socklen_t lg = sizeof(struct sockaddr_in) ;
-	int dSC1 = accept(dS, (struct sockaddr*) &aC,&lg);
-	if (dSC1 < 0) {
-		perror("Erreur lors de l'acceptation du client 1\n");
+	while (1)
+	{
+		int dSC;
+		// Vérifions si on peut accepter un client
+		// On attends la disponibilité du sémaphore
+		sem_wait(&semaphore);
+		
+		// Acceptons une connexion
+		struct sockaddr_in aC;
+		socklen_t lg = sizeof(struct sockaddr_in);
+		dSC = accept(dS, (struct sockaddr *)&aC, &lg);
+		if (dSC < 0)
+		{
+			perror("Problème lors de l'acceptation du client\n");
+			exit(-1);
+		}
+		printf("Client %ld connecté\n", nbClient);
+
+		// Réception du pseudo
+		char *pseudo = (char *)malloc(sizeof(char) * 100);
+		receiving(dSC, pseudo, sizeof(char) * 12);
+		pseudo = strtok(pseudo, "\n");
+		
+		while (verifPseudo(pseudo))
+		{
+			send(dSC, "Pseudo déjà existant\n",strlen("Pseudo déjà existant\n"), 0);
+			receiving(dSC, pseudo, sizeof(char) * 12);
+			pseudo = strtok(pseudo, "\n");
+		}
+
+		
+		// Enregistrement du client
+		long numClient = giveNumClient();
+		pthread_mutex_lock(&mutex);
+		tabClient[numClient].isOccupied = 1;
+		tabClient[numClient].dSC = dSC;
+		tabClient[numClient].pseudo = (char *)malloc(sizeof(char) * 12);
+		strcpy(tabClient[numClient].pseudo, pseudo);
+		pthread_mutex_unlock(&mutex);
+
+		// On envoie un message pour dire au client qu'il est bien connecté
+		char *repServ = (char *)malloc(sizeof(char) * 100);
+		repServ = "Entrer /aide pour avoir la liste des commandes disponibles\n";
+		sendingDM(pseudo, repServ);
+
+		// On envoie un message pour avertir les autres clients de l'arrivée du nouveau client
+		strcat(pseudo, " a rejoint la communication\n");
+		sending(dSC, pseudo);
+		free(pseudo);
+
+		//_____________________ Communication _____________________
+		if (pthread_create(&tabThread[numClient], NULL, communication, (void *)numClient) == -1)
+		{
+			perror("Erreur thread create");
+		}
+
+		// On a un client en plus sur le serveur, on incrémente
+		nbClient += 1;
+		printf("Clients connectés : %ld\n", nbClient);
 	}
-	printf("Client 1 Connecté\n");
-
-	int dSC2 = accept(dS, (struct sockaddr*) &aC,&lg);
-	if (dSC2 < 0) {
-		perror("Erreur lors de l'acceptation du client 2\n");
-	}
-	printf("Client 2 Connecté\n");
-
-	struct args *adr = (struct args *)malloc(sizeof(struct args));
-    adr->adrC1 = dSC1;
-    adr->adrC2 = dSC2;
-
-	pthread_t thread_client1To2;
-	pthread_t thread_client2To1;
-	pthread_create(&thread_client1To2, NULL, client1To2, (void *) adr);
-	pthread_create(&thread_client2To1, NULL, client2To1, (void *) adr);
-	
-
-	pthread_join(thread_client1To2,0);
-	pthread_join(thread_client2To1,0);
-
-	shutdown(dSC1, 2) ;
-	shutdown(dSC2, 2) ;
-	shutdown(dS, 2) ;
+	shutdown(dS, 2);
+	sem_destroy(&semaphore);
 	printf("Fin du programme\n");
 }
